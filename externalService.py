9c4e5fb00898355baf473122c6563a9e1d24043d
Stefan Armbruster <stefan@armbruster-it.de>
2023-06-06 19:45:06 +0200

    neo4j https/bolt tls works

---
+++ externalService.py
@@ -0,0 +1,23 @@
+import http.server
+import ssl
+
+# Set up the server address and port
+address = ('', 8080)
+
+# Generate a self-signed certificate
+certfile = 'certificate.crt'
+keyfile = 'private.key'
+
+# Create an HTTP request handler
+handler = http.server.SimpleHTTPRequestHandler
+
+# Create an SSL context
+context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+context.load_cert_chain(certfile, keyfile)
+
+# Start the web server
+httpd = http.server.HTTPServer(address, handler)
+httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+
+print("Starting the server on https://localhost:8080...")
+httpd.serve_forever()
--- neo4j.sh
+++ neo4j.sh
@@ -2,11 +2,49 @@
 
 #!/bin/bash
 
-# Generate a private key
-openssl genpkey -algorithm RSA -out private.key
+KEY_FILE=${PWD}/private.key
+CERT_FILE=${PWD}/certificate.crt
 
-# Generate a certificate signing request (CSR)
-openssl req -new -key private.key -out certificate.csr
+if [ ! -f "${KEY_FILE}" ]; then
+    # Generate a private key
+    openssl genpkey -algorithm RSA -out $KEY_FILE
+fi
 
-# Generate a self-signed certificate using the CSR and private key
-openssl x509 -req -days 365 -in certificate.csr -signkey private.key -out certificate.crt
+if [ ! -f "${CERT_FILE}" ]; then
+    # Generate a certificate signing request (CSR)
+    # TODO: replace CN=localhost with CN=www.mydomain.com
+    openssl req -new -key $KEY_FILE -subj "/C=DE/L=Munich/O=Company/CN=localhost" -out certificate.csr
+
+    # Generate a self-signed certificate using the CSR and private key
+    openssl x509 -req -days 365 -in certificate.csr -signkey $KEY_FILE -out $CERT_FILE
+fi
+
+# launch a python https webserver to simulate external service
+#python externalService.py &
+
+docker run --rm \
+--name neo4j44 \
+-e NEO4J_AUTH=neo4j/123 \
+-e NEO4J_ACCEPT_LICENSE_AGREEMENT=yes \
+-e NEO4J_dbms_connector_https_enabled=true \
+-e NEO4J_dbms_connector_http_enabled=false \
+-e NEO4J_dbms_connector_bolt_tls__level="REQUIRED" \
+-e NEO4J_dbms_ssl_policy_https_enabled="true" \
+-e NEO4J_dbms_ssl_policy_https_base__directory="certificates" \
+-e NEO4J_dbms_ssl_policy_bolt_enabled="true" \
+-e NEO4J_dbms_ssl_policy_bolt_base__directory="certificates" \
+-e NEO4JLABS_PLUGINS='["apoc"]' \
+-p 7474:7474 \
+-p 7473:7473 \
+-p 7687:7687 \
+-v $CERT_FILE:/var/lib/neo4j/certificates/public.crt \
+-v $KEY_FILE:/var/lib/neo4j/certificates/private.key \
+--user=$(id -u):$(id -g) \
+--add-host=host.docker.internal:host-gateway \
+neo4j:4.4.8-enterprise
+
+# to connect via cypher-shell use
+# cypher-shell -a "neo4j+ssc://localhost:7687" 
+
+# for apoc:
+#-e NEO4JLABS_PLUGINS='["apoc"]' 
